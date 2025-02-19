8964492337724a75918ffe76a7145c5b33189ad1
jotafra <jozinbrbrasil@gmail.com>
2025-02-19 16:11:48 -0300

    app_com_api_19_02

---
+++ src/App.js
@@ -0,0 +1,49 @@
+//import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+import Login from './screens/login';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Login></Login>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  box1:{
+    width:50,
+    height:50,
+    backgroundColor:'red'
+  },box2:{
+    width:50,
+    height:50,
+    backgroundColor:'green'
+  },box3:{
+    width:50,
+    height:50,
+    backgroundColor:'brown'
+  },box4:{
+    width:50,
+    height:50,
+    backgroundColor:'yellow'
+  },
+  row:{
+    flexDirection:'row',
+  },
+  text:{
+    fontSize:28,
+    fontWeight:"bold"
+  },
+  input:{
+    borderWidth: 1,
+    borderColor: "gray",
+    width: "80%",
+    padding:10,
+    marginVertical:10
+  }
+});
